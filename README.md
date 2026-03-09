34b1faea05ca73ee6d5abc0e4b6ad386d91edb3c
Ignacio Casso <ignacio.casso@mailbox.org>
2026-03-09 19:29:18 +0100

    copy already existing code

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# breaking-bad
-Incremental adoption of modal editing in Emacs
+# Breaking Bad
+
+This is yet another modal editing package for Emacs. The motivation
+behind it is that I want to start using modal editing, ideally with
+Vim bindings, but switching all at once is too overwhelming. Instead,
+I'm writing my own package to be able to transition step by step and
+learn a bit about Emacs internals along the way. If the package is
+useful for someone else, all the better.
+
+## Modal editing advantages I want to replicate
+
+I don't know yet the full power of modal editing, but these are the
+features/advantages that I want to replicate
+
+- Different key bindings depending of context or mode. In particular,
+  free a lot of key bindings when you are not inserting text
+
+- Consistent bindings for verbs, targets and modifiers. So you are
+  able to perform NxM actions with N+M bindings
+
+- Vim bindings, which seem to me that are more widespread outside
+  Emacs than Emacs bindings
+
+## Other features specific to this package
+
+- I want to be able to have assign different commands to the same
+  binding in different major modes
+
+## Adoption levels
+
+I would like to have some variable that indicated the level of
+adoption and enable or disable a number of features depending on it,
+but that still requires some though. For now, adoption levels will
+just correspond to commits in this repository.
+
+### Level 1
+
+The goal of this level is to just get used to having separate normal
+and insert modes.
+
+This level introduces a minor mode for normal-mode, which can be
+enabled globally, and which remaps `self-insert-command` to a command
+that calls the function in the variable `bb-normal-binding-for-<key>`,
+which by default is just doing nothing. This is done in a way that
+still works for already existing remaps, like
+`org-self-insert-command`: if that command would end up
+self-inserting, it would do the new action instead, otherwise the
+previous remap takes preference. The binding `i` will enter
+insert-mode minor mode in the current buffer
+
+insert-mode is just normal Emacs behaviour but
+- `C-g` exits it and enters normal-mode again
+
+## Comparison with other packages
+
+### Evil mode
+
+This is the most famous package for Vim emulation, and the one I will
+try to use as inspiration for this package. The idea is that after
+completing this project, I should be able to just start using
+evil-mode instead of this package.
---
+++ breaking-bad.el
@@ -0,0 +1,104 @@
+;;; breaking-bad.el --- Incremental adoption of modal editing in Emacs
+
+;;; Commentary:
+
+;; More information is available in the README.md file.
+
+;;; Code:
+
+(defvar bb-normal-map (make-sparse-keymap)
+  "Keymap for my normal state.")
+
+(define-minor-mode bb-normal-mode
+  "My modal normal state."
+  :init-value nil
+  :lighter " N"
+  :keymap bb-normal-map)
+
+
+(defvar bb-excluded-modes nil)
+
+(defun bb--maybe-activate ()
+  (unless (or (minibufferp)
+              (member major-mode bb-excluded-modes))
+    (bb-normal-mode 1)))
+
+(define-globalized-minor-mode bb-global-mode
+  bb-normal-mode
+  bb--maybe-activate)
+
+
+(defun bb-normal-key-undefined (&rest args)
+  (interactive)
+  (funcall #'undefined))
+
+(defun bb-normal-key-variable (key)
+  (intern (concat "bb-normal-binding-for-" key)))
+
+(defun bb-self-insert-remap ()
+  (interactive)
+  (let* ((keys (this-command-keys-vector)) ; full key sequence as a vector
+         (key (key-description keys))
+         (key-variable (bb-normal-key-variable key))
+         (key-function (eval key-variable))
+         (arg current-prefix-arg)
+         (original-remap (command-remapping 'self-insert-command nil (delq bb-normal-map (current-active-maps)))))
+    (if key-function
+        (if arg
+            (apply key-function (list arg))
+          (apply key-function nil))
+      (if original-remap
+          (cl-letf (((symbol-function #'self-insert-command) #' bb-normal-key-undefined))
+            (call-interactively original-remap))
+        (funcall #'undefined)))))
+
+
+(keymap-set bb-normal-map "<remap> <self-insert-command>" #'bb-self-insert-remap)
+
+
+(dolist (key (mapcar (lambda (ch) (key-description (vector ch)))
+                     (number-sequence 32 126)))
+         (let ((key-variable (bb-normal-key-variable key)))
+           (set key-variable nil)))
+
+
+(defun bb-set-normal-key (key binding &optional forbid)
+  (set (bb-normal-key-variable key) binding)
+  (when (and forbid (symbolp binding))
+    (keymap-set bb-normal-map
+                (format "<remap> <%s>" (symbol-name binding))
+                (lambda () (interactive) (message "Use normal mode binding!!!")))))
+
+(bb-set-normal-key "i" (lambda (&rest args) (bb-normal-mode -1) (bb-insert-mode 1)))
+(bb-set-normal-key "h" #'backward-char)
+(bb-set-normal-key "j" #'next-line)
+(bb-set-normal-key "k" #'previous-line)
+(bb-set-normal-key "l" #'forward-char)
+
+(defun bb-set-normal-key-local (key binding)
+  (let ((key-var (bb-normal-key-variable key)))
+    (make-local-variable key-var)
+    (set key-var binding)))
+
+
+
+;;; insert-mode
+
+(defvar bb-insert-map (make-sparse-keymap)
+  "Keymap for my insert state.")
+
+(define-minor-mode bb-insert-mode
+  "My modal insert state."
+  :init-value nil
+  :lighter " N"
+  :keymap bb-insert-map)
+
+(define-key bb-insert-map (kbd "C-g")
+            (lambda () (interactive) (bb-insert-mode -1) (bb-normal-mode 1)))
+
+
+
+
+(provide 'breaking-bad)
+
+;;; breaking-bad.el ends here
